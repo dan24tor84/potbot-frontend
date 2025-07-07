df1dde2a86f9e982f5aca1fdc0e15392aea00d74
Dan <dan24tor84@gmail.com>
2025-07-07 05:34:36 -0700

    Create NavBar.js

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="logo">🌿 Pot Bot</div>
+      <ul className="nav-links">
+        <li className={location.pathname === '/' ? 'active' : ''}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={location.pathname === '/budbot' ? 'active' : ''}>
+          <Link to="/budbot">Bud Bot</Link>
+        </li>
+        <li className={location.pathname === '/grower' ? 'active' : ''}>
+          <Link to="/grower">Grower Mode</Link>
+        </li>
+        <li className={location.pathname === '/pro' ? 'active' : ''}>
+          <Link to="/pro">Pro Mode</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
