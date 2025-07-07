22e0d7d60abb5b5912623776c1b921d3f0cedbf3
Dan <dan24tor84@gmail.com>
2025-07-07 10:50:30 -0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.potbot.app',
+  appName: 'PotBot',
+  webDir: 'frontend/dist',
+  bundledWebRuntime: false
+};
+
+export default config;
