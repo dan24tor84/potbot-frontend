e2d505ed1c199ddf6ed53ff92f257ff8ec3619b5
dan24 <dan24tor84@gmail>
2025-07-13 22:21:53 -0700

    Finale merge fixes and staging PotBot frontend

---
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -0,0 +1,5 @@
+package com.rankyourdank.potbot;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rankyourdank.potbot',
+  appName: 'PotBot',
+  webDir: 'build'
+};
+
+export default config;
