d6a901d5a08ac1bce4842826743b19ce78e69f76
Dan <dan24tor84@gmail.com>
2025-07-15 02:48:43 -0700

    Update capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.rankyourdank.potbot',
-  appName: 'PotBot',
-  webDir: 'build'
+// capacitor.config.ts
+const config = {
+  appId: "com.potbot.app",
+  appName: "Pot Bot",
+  webDir: "../build", // Use "../build" if Android is in frontend/android
+  bundledWebRuntime: false,
+  android: {
+    path: "frontend/android"
+  }
 };
 
 export default config;
