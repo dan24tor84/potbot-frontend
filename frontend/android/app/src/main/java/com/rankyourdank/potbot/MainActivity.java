87a2bf1ad11f05d0bc94098df448cd05e265a9cd
dan24 <dan24tor84@gmail>
2025-07-17 13:19:57 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend/android/settings.gradle

--- frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -1,5 +1,27 @@
 package com.rankyourdank.potbot;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.plugin.camera.CameraPlugin;
+import com.getcapacitor.plugin.filesystem.FilesystemPlugin;
+import com.getcapacitor.plugin.geolocation.GeolocationPlugin;
+import com.getcapacitor.plugin.localnotifications.LocalNotificationsPlugin;
+import com.getcapacitor.plugin.pushnotifications.PushNotificationsPlugin;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            add(CameraPlugin.class);
+            add(FilesystemPlugin.class);
+            add(GeolocationPlugin.class);
+            add(LocalNotificationsPlugin.class);
+            add(PushNotificationsPlugin.class);
+        }});
+    }
+}
