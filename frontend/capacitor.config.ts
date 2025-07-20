11069fee701eeef851093479463a165392a90fe4
dan24 <dan24tor84@gmail>
2025-07-19 19:15:50 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend/android/app/build.gradle
    #	frontend/android/settings.gradle

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rankyourdank.potbot',
+  appName: 'PotBot',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
