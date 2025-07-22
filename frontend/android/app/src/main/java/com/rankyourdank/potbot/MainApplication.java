462a06e0b64327added40568aae686bde7cac1e2
Dan <dan24tor84@gmail.com>
2025-07-21 23:30:09 -0700

    Create MainApplication.java

---
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainApplication.java
@@ -0,0 +1,12 @@
+package com.rankyourdank.potbot;
+
+import android.app.Application;
+import com.getcapacitor.BridgeActivity;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // App-level initialization (if needed later)
+    }
+}
