9b122f5facc22d4afaaa29f847838c584907fd02
Dan <dan24tor84@gmail.com>
2025-07-21 23:24:31 -0700

    Update MainActivity.java

--- frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -1,15 +1,25 @@
 package com.rankyourdank.potbot;
 
-import android.os.Bundle; import com.getcapacitor.BridgeActivity; import com.getcapacitor.Plugin; import com.getcapacitor.plugin.camera.CameraPlugin; import com.getcapacitor.plugin.filesystem.FilesystemPlugin; import com.getcapacitor.plugin.geolocation.GeolocationPlugin; import com.getcapacitor.plugin.permissions.PermissionsPlugin; import java.util.ArrayList;
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.plugin.camera.CameraPlugin;
+import com.getcapacitor.plugin.filesystem.FilesystemPlugin;
+import com.getcapacitor.plugin.geolocation.GeolocationPlugin;
+import com.getcapacitor.plugin.permissions.PermissionsPlugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity { @Override public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-// Initialize Capacitor plugins
-this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-  add(CameraPlugin.class);
-  add(FilesystemPlugin.class);
-  add(GeolocationPlugin.class);
-  add(PermissionsPlugin.class);
-}});
-
-} }
+    // Initialize Capacitor plugins
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      add(CameraPlugin.class);
+      add(FilesystemPlugin.class);
+      add(GeolocationPlugin.class);
+      add(PermissionsPlugin.class);
+    }});
+  }
+}
