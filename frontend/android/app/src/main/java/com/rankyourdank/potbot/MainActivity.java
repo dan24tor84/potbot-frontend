e239020ba88f75adb9e175635c8d3cd1fd689d29
Dan <dan24tor84@gmail.com>
2025-07-23 18:48:01 -0700

    Update MainActivity.java

--- frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends BridgeActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Initialize Capacitor plugins
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       add(CameraPlugin.class);
       add(FilesystemPlugin.class);
