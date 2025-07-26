751a2c87e96458beb63a8c98de207942c70eccfe
Dan <dan24tor84@gmail.com>
2025-07-26 13:14:27 -0700

    Update MainActivity.java

--- frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
+++ frontend/android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -1,6 +1,5 @@
 package com.rankyourdank.potbot;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
@@ -10,18 +9,15 @@ import com.getcapacitor.community.geolocation.GeolocationPlugin;
 import com.getcapacitor.community.permissions.PermissionsPlugin;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        this.init(
-                savedInstanceState,
-                new Plugin[] {
-                        CameraPreview.class,
-                        FilePicker.class,
-                        GeolocationPlugin.class,
-                        PermissionsPlugin.class
-                }
-        );
-    }
+    this.init(savedInstanceState, new Plugin[] {
+      CameraPreview.class,
+      FilePicker.class,
+      GeolocationPlugin.class,
+      PermissionsPlugin.class
+    });
+  }
 }
