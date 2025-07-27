31f4467d08587cfc3ae85f9cd8f420ec49001bab
Dan <dan24tor84@gmail.com>
2025-07-27 03:38:12 -0700

    Install frontend commit

---
+++ android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rankyourdank.potbot;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import com.getcapacitor.plugin.camera.Camera;
+import com.getcapacitor.plugin.filesystem.Filesystem;
+import com.getcapacitor.plugin.geolocation.Geolocation;
+import com.getcapacitor.plugin.permissions.Permissions;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+            add(Camera.class);
+            add(Filesystem.class);
+            add(Geolocation.class);
+            add(Permissions.class);
+        }});
+    }
+}
