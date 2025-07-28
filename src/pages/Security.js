1f6b5100c66be706f5f4d1b26a32f5769befe8d6
Dan <dan24tor84@gmail.com>
2025-07-28 07:55:43 -0700

    Create Security.js

---
+++ src/pages/Security.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Security = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Security Notice</h1>
+      <p>
+        PotBot implements secure image upload and encrypted database storage.
+      </p>
+      <p>
+        API keys and user data are protected using environment variables and backend authentication.
+      </p>
+      <p>
+        We take security seriously. Report any concerns to: security@rankyourdank.com
+      </p>
+    </div>
+  );
+};
+
+export default Security;
