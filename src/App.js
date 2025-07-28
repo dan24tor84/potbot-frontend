490fdc0fd9f5f013951e2746db011bfdc3fa763b
Dan <dan24tor84@gmail.com>
2025-07-28 07:58:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+import Security from "./pages/Security";
+import Footer from "./components/Footer";
+// import other pages here as needed
 
-function App() {
+const App = () => {
   return (
-    <div style={{ textAlign: "center", paddingTop: "100px" }}>
-      <h1 style={{ color: "green" }}>🌿 Pot Bot is Alive and Growing 🌱</h1>
-      <p>If you're seeing this, your frontend is working.</p>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Add main content here */}
+        <Routes>
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/security" element={<Security />} />
+          {/* Add your other routes here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
