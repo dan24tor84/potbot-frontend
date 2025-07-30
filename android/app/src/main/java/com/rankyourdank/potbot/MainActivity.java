16d57ad43270c94992ff571a23c972b1e9a1014c
Dan <dan24tor84@gmail.com>
2025-07-29 23:42:20 -0700

    Update MainActivity.java

--- android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
+++ android/app/src/main/java/com/rankyourdank/potbot/MainActivity.java
@@ -4,23 +4,33 @@ import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
-import com.getcapacitor.plugin.camera.CameraPlugin;
-import com.getcapacitor.plugin.filesystem.FilesystemPlugin;
-import com.getcapacitor.plugin.geolocation.GeolocationPlugin;
-import com.getcapacitor.plugin.permissions.PermissionsPlugin;
+import com.getcapacitor.camera.CameraPlugin;
+import com.getcapacitor.filesystem.FilesystemPlugin;
+import com.getcapacitor.geolocation.GeolocationPlugin;
+import com.getcapacitor.haptics.HapticsPlugin;
+import com.getcapacitor.preferences.PreferencesPlugin;
+import com.getcapacitor.statusbar.StatusBarPlugin;
+import com.getcapacitor.permissions.PermissionsPlugin;
+import com.getcapacitor.toast.ToastPlugin;
 
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      add(CameraPlugin.class);
-      add(FilesystemPlugin.class);
-      add(GeolocationPlugin.class);
-      add(PermissionsPlugin.class);
-    }});
-  }
+        this.init(
+            new ArrayList<Class<? extends Plugin>>() {{
+                add(CameraPlugin.class);
+                add(FilesystemPlugin.class);
+                add(GeolocationPlugin.class);
+                add(HapticsPlugin.class);
+                add(PreferencesPlugin.class);
+                add(StatusBarPlugin.class);
+                add(PermissionsPlugin.class);
+                add(ToastPlugin.class);
+            }}
+        );
+    }
 }
