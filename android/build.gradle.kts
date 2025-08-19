a75e47f6a7b607a8352eb334953124ec75bdce1d
Dan <dan24tor84@gmail.com>
2025-08-19 10:49:10 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,11 @@
-// Top-level Gradle build file
+// Root-level build.gradle.kts
+
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
 
 allprojects {
     repositories {
@@ -7,7 +14,6 @@ allprojects {
     }
 }
 
-// Keep the app module evaluated before others that depend on it
 subprojects {
     project.evaluationDependsOn(":app")
 }
