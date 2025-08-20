6564d6abe6a133a3181280a23f8fc81ca3ecfbe8
Dan <dan24tor84@gmail.com>
2025-08-20 00:43:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ const WEB_ROOT = path.join(__dirname, 'build', 'web');
 
 app.use(express.static(WEB_ROOT, { maxAge: '7d', immutable: true }));
 app.get('*', (_, res) => res.sendFile(path.join(WEB_ROOT, 'index.html')));
+
 app.listen(PORT, () => console.log(`PotBot web listening on ${PORT}`));
